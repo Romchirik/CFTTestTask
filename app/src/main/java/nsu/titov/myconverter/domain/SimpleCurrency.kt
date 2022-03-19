8f02960a0eecc95e90a1945a6464ac35ad5b7777
Romchirik <r.titov@g.nsu.ru>
2022-03-19 12:08:22 +0700

    finished currency list feature

--- app/src/main/java/nsu/titov/myconverter/domain/SimpleCurrency.kt
+++ app/src/main/java/nsu/titov/myconverter/domain/SimpleCurrency.kt
@@ -2,6 +2,7 @@ package nsu.titov.myconverter.domain
 
 data class SimpleCurrency(
     val nominal: Int,
+    val charCode: String,
     val name: String,
     val currentValue: Double,
     val currencyTrend: CurrencyTrend
--- app/src/main/java/nsu/titov/myconverter/presentation/adapters/CurrencyListRecyclerAdapter.kt
+++ app/src/main/java/nsu/titov/myconverter/presentation/adapters/CurrencyListRecyclerAdapter.kt
@@ -1,28 +1,19 @@
 package nsu.titov.myconverter.presentation.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import nsu.titov.myconverter.R
 import nsu.titov.myconverter.databinding.CurrencyRateItemBinding
 import nsu.titov.myconverter.domain.CurrencyTrend
 import nsu.titov.myconverter.domain.SimpleCurrency
 
 
-class CurrencyListRecyclerAdapter : RecyclerView.Adapter<CurrencyListRecyclerAdapter.CurrencyRateViewHolder>() {
+class CurrencyListRecyclerAdapter :
+    RecyclerView.Adapter<CurrencyListRecyclerAdapter.CurrencyRateViewHolder>() {
 
-    private val currencyList: List<SimpleCurrency> = listOf(
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING),
-        SimpleCurrency(1, "", 2.0, CurrencyTrend.RISING)
-    )
+    private var currencyList: List<SimpleCurrency> = listOf()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyRateViewHolder {
@@ -33,19 +24,34 @@ class CurrencyListRecyclerAdapter : RecyclerView.Adapter<CurrencyListRecyclerAda
 
     override fun onBindViewHolder(holder: CurrencyRateViewHolder, position: Int) {
         val item = currencyList[position]
-
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
         return currencyList.size
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    fun setCurrencyList(list: List<SimpleCurrency>) {
+        currencyList = list
+        notifyDataSetChanged()
+    }
 
     class CurrencyRateViewHolder(private val binding: CurrencyRateItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(currencyData: SimpleCurrency) {
-            //TODO("Fill this with code")
+            binding.charCodeView.text = currencyData.charCode
+            binding.nominalNameView.text = "%s %s".format(currencyData.nominal, currencyData.name)
+            binding.valueView.text = "%.2lf".format(currencyData.currentValue)
+            binding.currencyTrendView.setImageResource(currencyTrendToPic(currencyData.currencyTrend))
+        }
+
+        private fun currencyTrendToPic(currencyTrend: CurrencyTrend): Int {
+            return when (currencyTrend) {
+                CurrencyTrend.RISING -> R.drawable.ic_baseline_keyboard_arrow_up_24
+                CurrencyTrend.FALLING -> R.drawable.ic_baseline_keyboard_arrow_down_24
+            }
         }
     }
 }
