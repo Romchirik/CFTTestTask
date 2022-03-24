6aa65143f431ac274e7c76ad798fe5312c84505d
Romchirik <r.titov@g.nsu.ru>
2022-03-25 02:14:56 +0700

    Added currency info caching

---
+++ app/src/main/java/nsu/titov/myconverter/data/models/CurrencyDto.kt
@@ -0,0 +1,21 @@
+package nsu.titov.myconverter.data.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "currencies_cached")
+data class CurrencyDto(
+    @PrimaryKey
+    val id: String,
+    @ColumnInfo(name = "num_code")
+    val numCode: Int,
+    @ColumnInfo(name = "char_code")
+    val charCode: String,
+    val nominal: Int,
+    val name: String,
+    val value: Double,
+    @ColumnInfo(name = "previous_value")
+    val previousValue: Double
+)
